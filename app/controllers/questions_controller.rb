fd96f390422793f148853d03551d45b4e40861a7
Shivraj <itscool.sid@gmail.com>
2018-03-13 15:34:19 +0530

    added options to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,6 +16,7 @@ class QuestionsController < ApplicationController
   # GET /questions/new
   def new
     @question = @quiz.questions.build
+    4.times { @question.possible_answers.build }
   end
 
   # GET /questions/1/edit
@@ -70,7 +71,7 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:title, :kind, :quiz_id)
+      params.require(:question).permit(:title, :kind, :quiz_id, possible_answers_attributes: [:id, :title])
     end
     def set_quiz
       @quiz = Quiz.find(params[:quiz_id])
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :quiz
+  has_many :possible_answers
+  accepts_nested_attributes_for :possible_answers, reject_if: :all_blank, allow_destroy: true
+
 end
