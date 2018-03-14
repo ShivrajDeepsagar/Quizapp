fc36c922065a644b6c8e20eef95a6f107ef5b71b
Shivraj <itscool.sid@gmail.com>
2018-03-14 17:23:38 +0530

    destroy issue

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :reply
   belongs_to :question
-  belongs_to :possible_answer
+  belongs_to :possible_answer, dependent: :destroy
 end
--- app/models/possible_answer.rb
+++ app/models/possible_answer.rb
@@ -1,3 +1,3 @@
 class PossibleAnswer < ActiveRecord::Base
-  belongs_to :question
+  belongs_to :question, dependent: :destroy
 end
