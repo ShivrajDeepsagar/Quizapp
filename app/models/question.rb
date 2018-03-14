a93d0216cf7ef401a2c7ea315a4841f214c43b66
Shivraj <itscool.sid@gmail.com>
2018-03-14 17:16:31 +0530

    destroy issue

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :quiz
-  has_many :possible_answers
+  has_many :possible_answers, dependent: :destroy
   has_many :answers
   accepts_nested_attributes_for :possible_answers, reject_if: :all_blank, allow_destroy: true
 
